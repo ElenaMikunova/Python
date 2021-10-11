2fe9a250dd9ff9cff72666a3cb5aa1d1caf42d2e
ElenaMikunova <91980864+ElenaMikunova@users.noreply.github.com>
2021-10-11 14:45:16 +0300

    Delete 4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Введите число: "))
-max_n = 0
-while number // 10 > 0:
-    if number % 10 >= max_n:
-        max_n = number % 10
-    number = number // 10
-if number % 10 >= max_n:
-    max_n = number % 10
-print("Самая большая цифра в числе - ", max_n)
